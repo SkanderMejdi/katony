f3a47630a76d31dc0b0514e953eb3e6afcad4958
Skander Mejdi <mejdi.skander@gmail.com>
2019-01-10 15:29:23 +0100

    test:

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
-import { D, C, H, S, highCard } from "./index";
+import { D, C, H, S, highCard, pair } from "./index";
 
-const hand1 = [
+const handOnlyWithHighCard = [
   { value: 2, color: H },
   { value: 3, color: D },
   { value: 5, color: S },
@@ -9,5 +9,18 @@ const hand1 = [
 ];
 
 it("returns the highest card of a hand", () => {
-  expect(highCard(hand1)).toEqual({ value: 13, color: D });
+  expect(highCard(handOnlyWithHighCard)).toEqual({ value: 13, color: D });
 });
+
+const handWithPair = [
+  { value: 2, color: H },
+  { value: 3, color: D },
+  { value: 2, color: S },
+  { value: 9, color: C },
+  { value: 13, color: D }
+];
+
+it("returns true if the hand has a pair", () => {
+  expect(pair(handWithPair).toBeTrue());
+  expect(pair(handOnlyWithHighCard).toBeTrue());
+})
