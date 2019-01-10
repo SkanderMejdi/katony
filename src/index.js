ad44e5be5ffc081f20d7b1af8e4231b36054bf3c
Taskim <tony@evaneos.com>
2019-01-10 15:05:54 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export const H = 'HEART';
+export const D = 'DIAMOND';
+export const S = 'SPARE';
+export const C = 'CLUB';
--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,13 @@
-it('Test green', () => {
+import { D, C, H, S } from "./index";
+
+const hand1 = [
+  { value: 2, color: H },
+  { value: 3, color: D },
+  { value: 5, color: S },
+  { value: 9, color: C },
+  { value: 13, color: D }
+];
+
+it("returns the highest card of a hand", () => {
   expect(1).toBe(1);
 });
